0d1f2a84da38628a9121b7bbd1fa24a17f80de04
George Campbell <campbell.goe@gmail.com>
2025-08-03 21:03:20 +0100

    minor deploy change

--- scripts/georgeDeploy.sh
+++ scripts/georgeDeploy.sh
@@ -4,3 +4,5 @@ docker build -t aphid/threat-weaver .
 docker tag aphid/threat-weaver:latest 791607200561.dkr.ecr.eu-west-1.amazonaws.com/aphid/threat-weaver:latest
 
 docker push 791607200561.dkr.ecr.eu-west-1.amazonaws.com/aphid/threat-weaver:latest
+
+aws ecs update-service --cluster earnest-frog-i1wnp0 --service threat-composer-weaver-task-definition-service-gn2xv1ih --force-new-deployment
